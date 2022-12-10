884c776a73313ad23c866d64c81e5188f647938b
Gyuhui Kwon <gyuhuikwon@Gyuhuis-MacBook-Air.local>
2022-12-11 03:05:00 +0900

    add multi_linear, edit single_linear, and question about history

--- WithoutTorch1.py
+++ WithoutTorch1.py
@@ -2,15 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 print("Step1: Development Single Linear Regression Algorithm")
-
 #input_array, target_array will be 1-d array
 #paramaters = weight, bias will be scalar
 #input_array=RM, target_array=MEDV, predict_array=예측한 출력값
+
 def linear_regression_1D(input_array, target_array, epoch):
-    weight, bias = 0, 0 #intialize weight & bias = 0
+    weight, bias = 3,3 #intialize weight & bias = 3
     loss = 0 #initialize loss =0
-    lr = 1e-4 #learning rate
-    print(f"initial weight, bias, loss: {weight}, {bias}, {loss}")
+    lr = 1e-3
+    print(f"initial : weight, bias, loss: {weight}, {bias}, {loss}")
 
     for i in range(0, epoch):
         #print(input_array)
@@ -18,32 +18,31 @@ def linear_regression_1D(input_array, target_array, epoch):
         #print(predict_array)
         diff_array = predict_array - target_array
         #print(diff_array)
-        gradient_weight = np.sum(2*input_array*diff_array) / len(diff_array)
+        gradient_weight = np.sum(2*input_array*diff_array) / len(diff_array) 
         #print(gradient_weight)
         gradient_bias = np.sum(2*diff_array) / len(diff_array)
         #print(gradient_bias)
         weight -= (lr * gradient_weight)
         bias -= (lr * gradient_bias)
         loss = lossfunction(diff_array)
-
-    #record history
+        history=np.zeros((0,2))
+        #accuracy(R^2 결정계수)
+        target_mean=target_array.mean()
+        acc=1-((target_array-predict_array)**2).sum()/((target_array-target_mean)**2).sum()
+        #??gradient 초기화시키고 다시 위로 올라가서 시작 #0으로 만들어주고 반복하게끔
+        print(f"final : weight, bias, loss: {weight:.4f}, {bias:.4f}, {loss:.4f}")
+        print(f"acc : {acc:.4f}")
+        return weight, bias, loss
     
-    
-    print(f"final weight, bias, loss: {weight:.4f}, {bias:.4f}, {loss:.4f}")
-    return weight, bias, loss
-
-### pulling data function
 def get_data(): 
     number_of_rooms_list, price_list = [], []
-
     with open("BostonData1.txt", "r") as f: #read text file as 'read mode'
              for lines in f: 
                 a = lines[0:5]
                 b = lines[5:10]
-                number_of_rooms_list.append(float(a.replace(" ", "")))
+                number_of_rooms_list.append(float(a.replace(" ", ""))) #부동소수점 자료형
                 price_list.append(float(b.replace(" ", ""))) #still 'list'
-
-    input_array = np.array(number_of_rooms_list) #행렬 형태로 변형
+    input_array = np.array(number_of_rooms_list) #change to array
     target_array = np.array(price_list)
     return input_array, target_array
 
@@ -57,24 +56,27 @@ def lossfunction(diff_array):
     loss = np.sum((diff_array)**2)/len(diff_array)
     return loss
 
-
-#
-if __name__ == "__main__": 
+history=np.zeros((0,2))
+#__name__ : interpreter 글로벌 변수 
+if __name__ == "__main__": #인터프리터에서 직접 실행했을 경우에만 if문을 실행해라(import 말고)
     number_of_rooms, prices = get_data()
-    epoch = 3000
-
+    epoch = 15000
     #print(number_of_rooms)
-    print(prices)
-    #print(len(number_of_rooms))
-
+    #print(prices)
     weight, bias, loss = linear_regression_1D(number_of_rooms, prices, epoch)
-
-    #
+    #draw result graph
     plt.plot(number_of_rooms, prices, ".", label="target")
     plt.plot(number_of_rooms, predict(number_of_rooms, weight, bias), ".", label="predict")
-    plt.savefig("test.png")
-
+    plt.savefig("result.png")
+    #??100회마다 기록
+    if (epoch%100 == 0) :
+        history=np.vstack((history, np.array([epoch, loss.item()])))
+        print(f'epoch : {epoch} / loss : {loss : .4f}')
+        
+    
 #loss graph
-
-#accuracy graph 
-
+plt.plot(history[:,0], history[:,1],'b')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.title('learning graph(loss)')
+plt.savefig("loss graph.png")
--- WithoutTorch2.py
+++ WithoutTorch2.py
@@ -7,3 +7,77 @@ print("Step2: Development Multi Linear Regression Algorithm")
 #paramaters = weight, bias will be matrix, vector
 #input_array1=RM, input_array2=CRIM , target_array=MEDV, predict_array=예측한 출력값
 
+def linear_regression_2D(input_array, target_array, epoch):
+    #intialize weight & bias
+    weight, bias = np.array([[1],[1]]), 1 #2차원 열벡터, 스칼라
+    loss = 0 #initialize loss =0
+    lr = 1e-3
+    print(f"initial : weight, bias, loss: {weight}, {bias}, {loss}")
+
+    for i in range(0, epoch):
+        predict_array = predict(input_array, weight, bias)
+        #print(predict_array)
+        diff_array = predict_array - target_array
+        #print(diff_array)
+        gradient_weight = np.sum(2*input_array*diff_array) / len(diff_array) 
+        #print(gradient_weight)
+        gradient_bias = np.sum(2*diff_array) / len(diff_array)
+        #print(gradient_bias)
+        weight -= (lr * gradient_weight)
+        bias -= (lr * gradient_bias)
+        loss = lossfunction(diff_array)
+        history=np.zeros((0,2))
+        #accuracy(R^2 결정계수)
+        target_mean=target_array.mean()
+        acc=1-((target_array-predict_array)**2).sum()/((target_array-target_mean)**2).sum()
+         #??gradient 초기화시키고 다시 위로 올라가서 시작 #0으로 만들어주고 반복하게끔
+        print(f"final : weight, bias, loss: {weight:.4f}, {bias:.4f}, {loss:.4f}")
+        print(f"acc : {acc:.4f}")
+        return weight, bias, loss
+    
+def get_data(): 
+    number_of_rooms_list, price_list, crime_rate_list = [], [], []
+    with open("BostonData1.txt", "r") as f: #read text file as 'read mode'
+            for lines in f: 
+                a = lines[0:5]
+                b = lines[5:10]
+                RM = number_of_rooms_list.append(float(a.replace(" ", ""))) #부동소수점 자료형
+                MEDV = price_list.append(float(b.replace(" ", ""))) #still 'list'
+    with open("BostonData2.txt", "r") as f:
+            for lines in f:
+                c = lines[3:10]
+                CRIM = crime_rate_list.append(float(c))
+    input_array = np.c_[np.array(RM), np.array(CRIM)] #change to array, 열 추가
+    target_array = np.array(MEDV)
+    return input_array, target_array
+    
+
+def predict(input_array, weight, bias):
+    predict_array = weight.dot(input_array) + bias
+    return predict_array
+
+
+def lossfunction(diff_array):
+    loss = np.sum((diff_array)**2)/len(diff_array)
+    return loss
+
+
+#__name__ : interpreter 글로벌 변수 
+if __name__ == "__main__": #인터프리터에서 직접 실행했을 경우에만 if문을 실행해라(import 말고)
+    RM_CRIM, prices = get_data()
+    epoch = 15000
+    #print(RM_CRIM)
+    #print(prices)
+    weight, bias, loss = linear_regression_2D(RM_CRIM, prices, epoch)
+    #draw result graph
+    plt.plot(RM_CRIM, prices, ".", label="target")
+    plt.plot(RM_CRIM, predict(RM_CRIM, weight, bias), ".", label="predict")
+    plt.savefig("result.png")
+    
+#loss graph
+plt.plot(history[:,0], history[:,1],'b')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.title('learning graph(loss)')
+plt.savefig("loss graph.png")
+
